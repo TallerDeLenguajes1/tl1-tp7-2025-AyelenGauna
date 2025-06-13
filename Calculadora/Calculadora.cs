4a9cfd034cec41ac03740b928c7412e2eecc6ab7
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-06-12 23:21:05 -0300

    Creo el archivo Calculadora.cs donde defino un namespace "EspacioCalculadora" : dentro de ese espacio se creó una clase "Calculadora" para encadenar operaciones sobre un mismo resultado

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -2,35 +2,41 @@ namespace EspacioCalculadora
 {
     public class Calculadora
     {
+        private double resultado;
+        public double Resultado { get => resultado; } 
         private double dato;
         public void Sumar(double termino)
         {
             dato += termino;
+            resultado = dato;
         }
         public void Restar(double termino)
         {
             dato -= termino;
+            resultado = dato;
         }
         public void Multiplicar(double termino)
         {
             dato *= termino;
+            resultado = dato;
         }
         public void Dividir(double termino)
         {
             if (termino != 0)
             {
                 dato /= termino;
+                resultado = dato;
             }
             else
             {
                 Console.WriteLine("Error: No se puede dividir por cero.");
             }
         }
-        public void Limpiar(double termino)
+        public void Limpiar()
         {
             dato = 0;
+            resultado = 0;
         }
-        private double resultado;
-        public double Resultado { get => resultado; } 
+        
     }
 }
